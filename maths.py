b06973c070bcbe403e6b3de88c204c665adfcefb
mtkhan7 <mtkhann7@gmail.com>
2020-02-21 12:35:59 +0000

    first commit

---
+++ maths.py
@@ -0,0 +1,31 @@
+
+#multiplication
+num1 = 3
+num2 = 4
+print(num1 * num2)
+
+#addition 
+num3 = 10
+num4 = 20
+print(num3 + num4)
+
+#division
+num5 = 4
+num6 = 2
+print(num5/num6)
+
+#remainder - modulo 
+num7 = 15
+num8 = 5
+print(num7 % num8)
+
+#substraction 
+num9 = 10
+num10 = 5
+print(num9 - num10)
+
+#addition assignment operator prints the value of both variables
+num11 = 10
+num12 =+ 5
+num13 = num11 + num12
+print(num13)
